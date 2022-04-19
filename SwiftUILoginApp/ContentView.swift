e0cabd1def24972666e6f6e5ebccd9f949244bcd
saxtime <saxtime@yandex.ru>
2022-04-19 13:19:37 +0300

    Initial Commit

---
+++ SwiftUILoginApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUILoginApp
+//
+//  Created by Андрей Василевский on 4/19/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUILoginApp/SwiftUILoginAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILoginAppApp.swift
+//  SwiftUILoginApp
+//
+//  Created by Андрей Василевский on 4/19/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILoginAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
